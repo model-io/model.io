a8d14d79a60d07fdfbd21682a6980be229a18dcf
Stephan Hoyer <ste.hoyer@gmail.com>
2014-02-28 12:25:26 +0100

    Clean up examples

--- example/express/app.js
+++ example/express/app.js
@@ -6,7 +6,8 @@ app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 //register client js
-app.use(express.static(__dirname + '/../../client/'));
+app.use(express.static(__dirname + '/../../test/client/'));
+app.use(express.static(__dirname + '/../../'));
 app.get('/', function(req, res) {
   res.render('index');
 });
--- example/express/index.js
+++ example/express/index.js
@@ -3,3 +3,7 @@ var models = require('./models');
 var serverIO = require('../../index');
 
 server = serverIO(app, models).listen(3000);
+
+setInterval(function() {
+  models.Dog.onBirth.dispatch(Math.random());
+}, 2000);
