7292842a82693af98a9f9982aa48f260f6ce8f8b
Stephan Hoyer <ste.hoyer@gmail.com>
2014-02-28 11:52:44 +0100

    Reorganize repo

--- test/server/app.js
+++ test/server/app.js
@@ -5,8 +5,8 @@ var app = express();
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 
-//register client js
-app.use(express.static(__dirname + '/../../client/'));
+app.use(express.static(__dirname + '/../client/'));
+app.use(express.static(__dirname + '/../..'));
 app.get('/', function(req, res) {
   res.render('index');
 });
