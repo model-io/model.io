79c9bde954c8bbe881b2e3bd50eddc4dd9399d80
Stephan Hoyer <ste.hoyer@gmail.com>
2014-05-19 15:16:38 +0200

    Make express example work again

--- example/express/index.js
+++ example/express/index.js
@@ -1,8 +1,19 @@
+var http = require('http');
+
+var sockjs = require('sockjs');
+var Channel = require('sock-channels');
+
 var app = require('./app');
 var models = require('./models');
 var serverIO = require('../../index');
 
-server = serverIO(app, models).listen(3000);
+var server = http.Server(app);
+var ws = sockjs.createServer();
+ws.installHandlers(server, {prefix:'/ws'});
+var baseCh = new Channel(ws, 'model.io');
+
+serverIO(baseCh, models)
+server.listen(3000);
 
 setInterval(function() {
   models.Dog.onBirth.dispatch(Math.random());
